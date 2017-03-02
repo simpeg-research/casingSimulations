20c1c12d2163e4bebe718b5b62dca27d71a6d268
Lindsey Heagy <lindseyheagy@gmail.com>
2017-03-01 21:41:17 -0800

    update tests

--- docs/conf.py
+++ docs/conf.py
@@ -20,7 +20,6 @@ import os
 import sys
 sys.path.append('../')
 sys.path.append('../tests')
-sys.path.append('../notebooks')
 
 # -- General configuration ------------------------------------------------
 
