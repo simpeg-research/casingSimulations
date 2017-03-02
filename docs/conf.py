556970099b4e32c4314106012f3011a23a602554
Lindsey Heagy <lindseyheagy@gmail.com>
2017-03-01 21:18:41 -0800

    add tests dir to path

--- docs/conf.py
+++ docs/conf.py
@@ -19,6 +19,7 @@
 import os
 import sys
 sys.path.append('../')
+sys.path.append('../tests')
 sys.path.append('../notebooks')
 
 # -- General configuration ------------------------------------------------
