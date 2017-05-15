97caaa7159da54c54666b07c5eef366e436cb37f
Lindsey Heagy <lindseyheagy@gmail.com>
2017-05-15 08:54:44 -0700

    Bump version: 0.0.1 → 0.0.2

--- casingSimulations/info.py
+++ casingSimulations/info.py
@@ -1,4 +1,4 @@
-__version__   = '0.0.1'
+__version__   = '0.0.2'
 __author__    = 'Lindsey Heagy'
 __license__   = 'MIT'
 __copyright__ = 'Copyright 2017 Lindsey Heagy'
--- setup.py
+++ setup.py
@@ -28,7 +28,7 @@ with open("README.rst") as f:
 
 setup(
     name="CasingSimulations",
-    version="0.0.1",
+    version="0.0.2",
     packages=find_packages(),
     install_requires=[
         'numpy>=1.7',
